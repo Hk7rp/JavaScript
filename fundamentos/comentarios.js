1123c9971c65cd38664952a2364a8e11c17deb95
Alves <hkrpedro@gmail.com>
2021-01-30 21:16:49 -0300

    primeiro dia de JS, 2 horas de aula

---
+++ fundamentos/comentarios.js
@@ -0,0 +1,15 @@
+// Comentário de 1 linha
+console.log("Linha 1")
+
+/*
+ Comentário de 
+ multiplas linhas
+*/
+
+console.log('Linha 2')
+
+/*
+ * Comentário de
+ * multiplas linhas
+*/
+console.log('Linha 3')
