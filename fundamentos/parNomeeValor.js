8a22147d0f40093b3cd2a5a2f003606abdcfafb4
Alves <hkrpedro@gmail.com>
2021-02-15 15:04:23 -0300

    mais umas aulas

---
+++ fundamentos/parNomeeValor.js
@@ -0,0 +1,21 @@
+const saudacao = 'Opa'// Contexto léxico 1
+
+function exec(){
+    const saudacao = 'Faaaala' // Contexto léxico 2
+    return saudacao
+}
+
+// Objetos são grupos aninhados de pares nomes/valor
+const cliente = {
+    nome: 'Pedro',
+    idade: 32,
+    peso: 90,
+    endereco: {
+        logradouro: 'Rua muito legal',
+        numero: 123
+    }
+}
+
+console.log(saudacao)
+console.log(exec())
+console.log(cliente)
